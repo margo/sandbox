d00c0026badcf5585de7a4cb4818f0d1943bbe1c
nitparihar <Nitin@cg.com>
2025-11-04 12:39:00 +0530

    changed cli from http to https

--- poc/wfm/cli/nbi.go
+++ poc/wfm/cli/nbi.go
@@ -98,7 +98,7 @@ func NewNbiHTTPCli(host string, port uint16, nbiBasePath *string, opts ...WFMCli
 
 	cli := &NbiApiClient{
 		serverAddress: fmt.Sprintf("%s:%d", host, port),
-		nbiBaseURL:    fmt.Sprintf("http://%s:%d/%s", host, port, nbiBaseURLPath),
+		nbiBaseURL:    fmt.Sprintf("https://%s:%d/%s", host, port, nbiBaseURLPath),
 		timeout:       nbiDefaultTimeout,
 		logger:        log.Default(),
 	}

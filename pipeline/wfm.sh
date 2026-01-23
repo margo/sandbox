0cbb954a4754e8fa5f9b31d3a9d7419c2e860a91
sanjujunnuthula <sanjuvardhan.junnuthula@capgemini.com>
2026-01-23 08:37:06 +0000

    shallow clone changes

--- pipeline/wfm.sh
+++ pipeline/wfm.sh
@@ -332,15 +332,24 @@ clone_symphony_repo() {
     rm -rf "$HOME/symphony"     
     echo "Cloning symphony branch: $SYMPHONY_BRANCH"
     if [[ -n "$GITHUB_USER" && -n "$GITHUB_TOKEN" ]]; then 
-      git clone --depth 1 "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/margo/symphony.git" "$HOME/symphony"
+      git clone \
+          --branch "$SYMPHONY_BRANCH" \
+          --single-branch \
+          --depth 1 \
+          "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/margo/symphony.git" \
+           "$HOME/symphony"
     else
-      git clone --depth 1 "https://github.com/margo/symphony.git" "$HOME/symphony"
+      git clone \
+          --branch "$SYMPHONY_BRANCH" \
+          --single-branch \
+          --depth 1 \
+          "https://github.com/margo/symphony.git" \
+           "$HOME/symphony"
     fi
   fi
   cd "$HOME/symphony"
-  git fetch --depth 1 --update-head-ok origin ${SYMPHONY_BRANCH}:${SYMPHONY_BRANCH}
-  git checkout ${SYMPHONY_BRANCH} || echo 'Branch ${SYMPHONY_BRANCH} not found'
-  git pull
+  # git fetch --depth 1 --update-head-ok origin ${SYMPHONY_BRANCH}:${SYMPHONY_BRANCH}
+  # git checkout ${SYMPHONY_BRANCH} || echo 'Branch ${SYMPHONY_BRANCH} not found'
   echo "✅ symphony repo checkout to branch ${SYMPHONY_BRANCH} done"
 }
 
@@ -350,15 +359,25 @@ clone_dev_repo() {
     rm -rf "$HOME/sandbox"
     echo "Cloning sandbox branch: $SANDBOX_REPO_BRANCH"
     if [[ -n "$GITHUB_USER" && -n "$GITHUB_TOKEN" ]]; then 
-      git clone --depth 1 "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/margo/sandbox.git"
+      git clone \
+          --branch "$SANDBOX_REPO_BRANCH" \
+          --single-branch \
+          --depth 1 \
+          "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/margo/sandbox.git" \
+          "$HOME/sandbox"      
     else
-      git clone --depth 1 "https://github.com/margo/sandbox.git"
+      git clone \
+          --branch "$SANDBOX_REPO_BRANCH" \
+          --single-branch \
+          --depth 1 \
+          "https://github.com/margo/sandbox.git" \
+          "$HOME/sandbox"
     fi
  fi
-  cd "$HOME/sandbox"
-  git fetch --depth 1 --update-head-ok origin ${SANDBOX_REPO_BRANCH}:${SANDBOX_REPO_BRANCH} || echo 'Unable to fetch ${SANDBOX_REPO_BRANCH}'
-  git checkout ${SANDBOX_REPO_BRANCH} || echo 'Branch ${SANDBOX_REPO_BRANCH} not found'
-  echo "sandbox repo checkout to branch ${SANDBOX_REPO_BRANCH} done"
+ cd "$HOME/sandbox"
+ # git fetch --depth 1 --update-head-ok origin ${SANDBOX_REPO_BRANCH}:${SANDBOX_REPO_BRANCH} || echo 'Unable to fetch ${SANDBOX_REPO_BRANCH}'
+ # git checkout ${SANDBOX_REPO_BRANCH} || echo 'Branch ${SANDBOX_REPO_BRANCH} not found'
+ echo "sandbox repo checkout to branch ${SANDBOX_REPO_BRANCH} done"
 }
 
 # ----------------------------

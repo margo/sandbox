e99b4aec41ecdae0354431e272bf2034a289c0e5
JUNNUTHULA <sanjuvardhan.junnuthula@capgemini.com>
2026-02-18 22:00:51 +0530

    changes on image manifest

--- pipeline/wfm-cli.sh
+++ pipeline/wfm-cli.sh
@@ -586,7 +586,7 @@ metadata:
 spec:
   sourceType: OCI_REPO
   source:
-    registryUrl: "http://${harbor_url}"
+    registryUrl: "http://registry:5000"
     repository: "${OCI_ORGANIZATION}/${package_repo}"
     tag: "latest"
     authentication:

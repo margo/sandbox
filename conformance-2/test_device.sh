5d2d49dd78adea0377ad190f9fa6848a8c0e5158
Nitin <nitin.parihar@capgemini.com>
2026-06-15 10:23:02 +0000

    chore: remove conformance-2 directory (merged into conformance)
    All functionality from conformance-2 has been consolidated into the
    single conformance/ directory. This directory is no longer needed.
    
    - Removes 108 tracked files from conformance-2/
    - Simplifies repository structure to single conformance folder
    - All WFM and Device Supplier components are now in conformance/

--- conformance-2/test_device.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Total args: $#"
-echo "Arg 1: $1"
-echo "Arg 2: $2"
-echo "Arg 2 is file: $([ -f "$2" ] && echo "YES" || echo "NO")"
-
-if [[ -f "$2" ]]; then
-    echo "File exists!"
-fi

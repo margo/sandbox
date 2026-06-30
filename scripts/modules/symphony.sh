67fa7b3bdbe06756d4816919f933c232b6ec86b7
sanjujunnuthula <sanjuvardhan.junnuthula@capgemini.com>
2026-06-30 10:31:39 +0000

    fix: sync ca-trust on restart
    Signed-off-by: sanjujunnuthula <sanjuvardhan.junnuthula@capgemini.com>

--- scripts/modules/symphony.sh
+++ scripts/modules/symphony.sh
@@ -39,6 +39,10 @@ ExecStart=/usr/bin/docker run --rm --name symphony-api-container \
     -v ${symphony_dir}:/configs \
     -e CONFIG=symphony-api-margo.json \
     ${SYMPHONY_IMAGE_REF}
+ExecStartPost=/bin/bash -c 'sleep 8; \
+/usr/bin/docker exec symphony-api-container sh -c "if [ -f /certificates/harbor-ca.crt ]; then mkdir -p /usr/local/share/ca-certificates && cp /certificates/harbor-ca.crt /usr/local/share/ca-certificates/harbor.crt && update-ca-certificates && echo Harbor CA trusted in Symphony container; fi"; \
+sleep 2; \
+docker restart symphony-api-container'
 ExecStop=/usr/bin/docker stop symphony-api-container
 TimeoutStartSec=0
 Restart=on-failure
@@ -161,4 +165,3 @@ start_symphony_api_container() {
       return 1
   fi
 }
-

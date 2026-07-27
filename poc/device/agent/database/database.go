a11852ee4a3c22d55d38d18f598f7a75b100bd3d
Pulkit Sharma <pulkit.d.sharma@capgemini.com>
2026-07-27 07:24:55 +0000

    fix(device-agent): removed redundant db update while setting desired state
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>

--- poc/device/agent/database/database.go
+++ poc/device/agent/database/database.go
@@ -301,6 +301,7 @@ func (db *Database) SetDesiredState(deploymentId string, state AppDeploymentStat
 
 	record, exists := db.deployments[deploymentId]
 	if !exists {
+		// initialise a record object
 		record = &DeploymentRecord{
 			AppID:               deploymentId,
 			DeploymentID:        deploymentId,
@@ -308,8 +309,7 @@ func (db *Database) SetDesiredState(deploymentId string, state AppDeploymentStat
 			Phase:               "pending",
 			LastUpdated:         time.Now(),
 		}
-		db.deployments[deploymentId] = record
-		db.notify(deploymentId, record, DeploymentChangeTypeDesiredStateAdded)
+		// db.notify(deploymentId, record, DeploymentChangeTypeDesiredStateAdded)
 	}
 
 	// Only update if actually different
@@ -324,6 +324,8 @@ func (db *Database) SetDesiredState(deploymentId string, state AppDeploymentStat
 	if state.URL != nil {
 		record.URL = *state.URL
 	}
+	// record should be updated in db after assigning all fields in record
+	db.deployments[deploymentId] = record
 
 	db.notify(deploymentId, record, DeploymentChangeTypeDesiredStateAdded)
 

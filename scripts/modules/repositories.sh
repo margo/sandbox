fffe22cb0be1d3dc540cdc03e2a947b98ccdbae9
Pulkit Sharma <pulkit.d.sharma@capgemini.com>
2026-09-07 09:42:31 +0000

    fix(scripts): corrected mis configuration path and mis config
    Signed-off-by: Pulkit Sharma <pulkit.d.sharma@capgemini.com>

--- scripts/modules/repositories.sh
+++ scripts/modules/repositories.sh
@@ -105,7 +105,7 @@ setup_mis_deployment() {
   if [[ -d "$HOME/mis-deployment" ]]; then
     echo "🗑️  Removing existing MIS deployment files..."
     rm -rf "$HOME/mis-deployment/docker-compose.yaml"
-    rm -rf "$HOME/mis-deployment/pkg/conf/configuration.json"
+    rm -rf "$HOME/mis-deployment/configuration.json"
   else
     echo "MIS deployment folder missing, certificates not generated. Please generate Factory Root CAs first..."
     return 1
@@ -127,7 +127,7 @@ setup_mis_deployment() {
   # Copy required files to $HOME/mis-deployment
   echo "📂 Copying MIS deployment files..."
   cp /tmp/sandbox/mis/docker-compose.yaml "$HOME/mis-deployment/"
-  cp /tmp/sandbox/mis/configuration.json "$HOME/mis-deployment/"
+  cp /tmp/sandbox/mis/pkg/conf/configuration.json "$HOME/mis-deployment/"
   
   # Cleanup
   rm -rf /tmp/sandbox
